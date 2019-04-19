09537a75410109fe79fa93ed2ddad01fa3f5a3b9
wjeyne-d <wjeyne-d@student.21-school.ru>
2019-04-19 20:46:01 +0300

    a

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -3,7 +3,7 @@
 $something = fopen("php://stdin", "r");
 while ($something) {
     echo("Enter a number: ");
-
+$something = fgets()
     if (feof($something)) {
         echo "\n";
         exit();
