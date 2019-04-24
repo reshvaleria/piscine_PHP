08abd2ed6018a5a8a47c552efa1561c44c62e6d7
wjeyne-d <wjeyne-d@student.21-school.ru>
2019-04-24 22:40:05 +0300

    rush00 started

--- rush00/index.php
+++ rush00/index.php
@@ -3,12 +3,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>Ароматы для дома</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <meta name="viewport" content="width=device-width">
+<!--    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Pacifico" rel="stylesheet">-->
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+    <meta charset="utf-8">
 </head>
 <body>
 <nav>
-
+    <ul>
+        <li><a href="#">Корзина</a></li>
+        <li><a href="#">Личный кабинет</a></li>
+        <li><a href="#">Контакты</a></li>
+        <li><a href="#">Доставка и оплата</a></li>
+        <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Каталог</a>
+            <div class="dropdown-content">
+                <a href="#">Свечи</a>
+                <a href="#">Диффузоры</a>
+                <a href="#">Аксессуары</a>
+            </div>
+        </li>
+        <li><a href="#">Главная</a></li>
+    </ul>
 </nav>
+<header class="main-header" id="top">
+     <span class="title">Уникальные ароматы, качество, натуральность</span>
+         <h1>Smell of Happiness</h1>
+    <img class="header_image" src="img/yankeecandle8.jpg" alt="Yankee Candle">
+</header>
+<ul class="main">
+    <li>dfghjkl
+    </li>
+    <li>cfghj</li>
+</ul>
+<footer>
+    <a href="#top">Вернуться к началу страницы &raquo;</a>
+</footer>
 </body>
 </html>
