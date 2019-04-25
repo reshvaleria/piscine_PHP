4a3ed33b0225de42a4f9e5167f1dc31c9ebf085f
Valeriia <reshvaleria@gmail.com>
2019-04-25 03:18:25 +0300

    a

--- rush00/index.php
+++ rush00/index.php
@@ -6,7 +6,6 @@
     <title>Ароматы для дома</title>
     <link rel="stylesheet" href="styles/style.css">
     <meta name="viewport" content="width=device-width">
-<!--    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300|Pacifico" rel="stylesheet">-->
     <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
 
     <meta charset="utf-8">
@@ -14,23 +13,24 @@
 <body>
 <nav>
     <ul>
-        <li><a href="#">Корзина</a></li>
+        <li><a href="basket.php">Корзина</a></li>
         <li><a href="#">Личный кабинет</a></li>
         <li><a href="#">Контакты</a></li>
         <li><a href="#">Доставка и оплата</a></li>
         <li class="dropdown">
             <a href="javascript:void(0)" class="dropbtn">Каталог</a>
             <div class="dropdown-content">
-                <a href="#">Свечи</a>
-                <a href="#">Диффузоры</a>
-                <a href="#">Аксессуары</a>
+                <a href="catalogue.php">Все товары</a>
+                <a href="catalogue.php">Свечи</a>
+                <a href="catalogue.php">Диффузоры</a>
+                <a href="catalogue.php">Аксессуары</a>
             </div>
         </li>
-        <li><a href="#">Главная</a></li>
+        <li class="logo"><a href="index.php">Smell of Happiness</a></li>
     </ul>
 </nav>
 <header class="main-header" id="top">
-     <span class="title">Уникальные ароматы, качество, натуральность</span>
+     <span class="title">Ароматы для дома</span>
          <h1>Smell of Happiness</h1>
     <img class="header_image" src="img/yankeecandle8.jpg" alt="Yankee Candle">
 </header>
@@ -40,6 +40,23 @@
     <li>cfghj</li>
 </ul>
 <footer>
+    <hr>
+    <div>
+    <p>Если у вас есть вопросы, свяжитесь с нами:</p>
+    <ul>
+        <li>+7 495 997 13 51</li>
+        <li>+7 916 524 67 00</li>
+        <li>sh-candles@mail.ru</li>
+    </ul>
+            <hr class="small_line" />
+    </div>
+   <p>Адрес</p>
+    <ul>
+        <li>Москва, ул. Воронцовская, 27/35 с1 (цокольный этаж)</li>
+    </ul>
+        <hr class="small_line" />
+    <p>ИП Белянкина Елена Юрьевна</p>
+    <p>ОГРНИП 318774600324841</p>
     <a href="#top">Вернуться к началу страницы &raquo;</a>
 </footer>
 </body>
